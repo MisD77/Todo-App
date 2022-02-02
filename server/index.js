77baffd1f36d16ad87d0e69e8c392b126faf355c
dikshya <dixyacharya@gmail.com>
2022-02-02 09:24:56 -0700

    minor changes

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-import express from "express";
-import Mongoose from "mongoose";
-
-const PORT = process.env.PORT || 3001;
+const express= require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 
-const connection = Mongoose.connect("mongodb://localhost/testdb");
-var db = Mongoose.connection
+var corsOptions = {origin: 'https://localhost:8081'};
+app.use(cors(corsOptions));
 
-connection.once("open", function(){
-console.log("connection with MongoDB was successful");
-});
+//parse requests of content-type - application/json
+app.use(bodyParser.json());
 
 //endpoints to interact with our react app
 app.get("/api", (req, res) => {
-    res.json({message: "Hello from server!"});
+    res.json({message: "Welcome to the to-do app!"});
 });
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT} ..`);
 });
