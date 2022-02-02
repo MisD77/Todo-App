508c8e101e8e3f70c592edd436f8846ffa1e9236
dikshya <dixyacharya@gmail.com>
2022-02-02 16:07:53 -0700

    Added TodoList to populate lists of todo-items

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import Header from "./components/Header";
 import AddToList from "./components/AddToList";
 import VarietyTask from "./components/VarietyTask";
-import TodoItem from "./components/TodoItem";
+import TodoList from "./components/TodoList";
 import "./index.css";
 
-
-
 function App() {
+
+  const[todo, setTodo] = useState([
+    {
+      id: 1,
+      text: "Grocery",
+    },
+    {
+      id: 2,
+      text: "Exercise",
+    },
+    {
+      id: 3,
+      text: "Study",
+    },
+  ])
+
   return (
     <>
       <Header />
       <AddToList />
       <VarietyTask />
-      <TodoItem/>
+      <TodoList todo={todo} />
     </>
   );
 }
