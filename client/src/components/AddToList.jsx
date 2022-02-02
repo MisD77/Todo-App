95050305fbc8b4f4ced2049c74bdc4ccaa0b8c35
dikshya <dixyacharya@gmail.com>
2022-02-02 11:39:07 -0700

    Added Form to take new todo input

---
+++ client/src/components/AddToList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AddToList() {
+  return (
+
+    <form action="#">
+        <input type="text" name="new-todo-add"/>
+        <input type="button" value="Add New Task"/>
+    </form>
+  );
+}
+
+export default AddToList;
