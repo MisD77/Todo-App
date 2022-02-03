7b0768c398a52a6040ea4feed4d500181d50b8d9
dikshya <dixyacharya@gmail.com>
2022-02-03 12:58:14 -0700

    Connected to Mongodb using docker-compose
    Created testDB.js file to create connection in separate file

--- server/index.js
+++ server/index.js
@@ -7,12 +7,15 @@ var corsOptions = {origin: 'https://localhost:8081'};
 app.use(cors(corsOptions));
 
 const mongoose = require('mongoose');
-//dixyaAchh, Dimbus.77
 
-const dbURI = "mongodb+srv://dixyaAchh:Dimbus.77@mycluster.eep8c.mongodb.net/todoDb?retryWrites=true&w=majority"
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => console.log("connected to db"))
-.catch((err) => console.log(err));
+//const dbURI = "mongodb+srv://dixyaAchh:Dimbus.77@mycluster.eep8c.mongodb.net/todoDb?retryWrites=true&w=majority"
+//mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true })
+//.then((result) => console.log("connected to db"))
+//.catch((err) => console.log(err));
+
+// Connecting to MongoDB
+var testDBRouter = require("../routes/testDB");
+app.use("/testDB", testDBRouter);
 
 //endpoints to interact with our react app
 app.get("/api", (req, res) => {
@@ -26,6 +29,23 @@ app.listen(PORT, () => {
 
 
 
-
-
+/*
+db.tasks.insertMany([ 
+    {
+        name: 'Exercie',
+        category: 'Lifestyle',
+        date: Date()
+    }, 
+    {
+        name: 'Grocery', 
+        category: 'Food',
+        date: Date()
+    }, 
+    {
+        name: 'Study',
+        category: 'Education', 
+        date: Date()
+    }
+])
+*/
 
