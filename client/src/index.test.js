5c66e7f1f13be7b69d7048e154507e5119aedcd4
dikshya <dixyacharya@gmail.com>
2022-02-04 10:12:19 -0700

    Added more test dependencies

--- client/src/index.test.js
+++ client/src/index.test.js
@@ -1,11 +1,3 @@
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-15";
 import React from "react";
-import Header from "./components/Header";
-Enzyme.configure({ adapter: new Adapter() });
-
-describe("Header", () => {
-  it("should render my component", () => {
-    expect(sum(2, 3)).toEqual(5);
-  });
-});
+import ReactDOM from "react-dom";
+import App from "./App";
