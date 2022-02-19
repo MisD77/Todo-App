8bfdce348cb22016033e22439c286a869e92b864
dikshya <dixyacharya@gmail.com>
2022-02-19 00:11:49 -0700

    MInor changes, resolved issues, Test setup for node and service layer added

--- client/babel.config.js
+++ client/babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   //presets: ["@babel/preset-env", "@babel/preset-react"],
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
+    ["@babel/preset-react", { targets: { node: "current" } }],
   ],
+  plugins: ["@babel/plugin-syntax-jsx"],
 };
